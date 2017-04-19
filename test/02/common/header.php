cfb8b78c94cddd2c482612c3fb0826449ff637f3
Snivview <dim_plymouth@yahoo.com>
2017-04-19 17:05:28 +0700

    prototype for kompas interactive project

--- test/02/common/header.php
+++ test/02/common/header.php
@@ -1,3 +1,10 @@
 <div id="headerArea" class="clearfix">
-    
+    <div class="wrap01">
+        <ul class="dispInline">
+            <li><a href="http://www.kompas.id" target="_blank">Kompas ID</a></li>
+            <li><a href="http://karier.kompas.id" target="_blank">Kompas Karier</a></li>
+            <li><a href="http://klasika.kompas.id" target="_blank">Klasika</a></li>
+            <li><a href="http://klasika.kompas.id" target="_blank">Advertorial</a></li>
+        </ul>
+    </div>
 </div>
--- test/02/index.php
+++ test/02/index.php
@@ -2,29 +2,60 @@
 <html>
 <head>
     <meta charset="utf-8">
-    <title>template</title>
-    <link rel="icon" href="img/tab_logo.png">
+    <title>The Spotlight</title>
     <meta name="viewport" content="initial-scale=1.0, user-scalable=yes" />
-    <meta name="description" content="template">
-    <meta name="keywords" content="template">
-    <meta name="author" content="Dimitri">
-    <link href="/template/common/css/base.css" rel="stylesheet">
+    <link href="common/css/base.css" rel="stylesheet">
 </head>
 
 <body>
     <div id="wrap">
 	<?php 
 		$path = $_SERVER['DOCUMENT_ROOT'];
-		$path .= "/template/common/header.php";
+		$path .= "/test/02/common/header.php";
 		include_once($path);
 	?>
-        <div id="mainArea" class="clearfix" style="padding-top:100px;">
-		</div><!-- /#mainArea -->
+        <div id="mainArea" class="clearfix">
+            <div class="topHeading">
+                <h1>The Spotlight</h1>
+                <h2>Rangkaian Peristiwa dalam Sebuah Sorotan</h2>
+            </div> <!-- /.topHeading -->
+            
+            <div class="wrap01">
+                <div class="container">
+                    <div class="block">
+                        <div><img src="img/spot01.png"></div>
+                        <h3>Suzuki Ignis</h3>
+                        <p>Lebih dari sekedar LCGC, City Car lansiran Suzuki ini mencoba mengusik pasar LCGC yang sebelumnya dikuasai pabrikan Jepang lainnya.</p>
+                    </div>
+                    <div class="block">
+                        <div><img src="img/spot02.jpg"></div>
+                        <h3>Longsor Wilis</h3>
+                        <p>Sebulan sudah warga lereng gunung Wilis dirundung rasa waswas akibat bergesernya tanah tempat mereka tinggal.</p>
+                    </div>
+                    <div class="block">
+                        <div><img src="img/spot03.png"></div>
+                        <h3>Pilkada Banten</h3>
+                        <p>Pasca sepeninggalan Ratu Atut yang diciduk lembaga KPK beberapa tahun lalu, warga banten masih dihadapkan pada sebuah pilihan sulit.</p>
+                    </div>
+                    <div class="block">
+                        <div><img src="img/spot02.jpg"></div>
+                        <h3>Longsor Wilis</h3>
+                        <p>Sebulan sudah warga lereng gunung Wilis dirundung rasa waswas akibat bergesernya tanah tempat mereka tinggal.</p>
+                    </div>
+                </div> <!-- /.container -->
+            </div> <!-- /.wrap01 -->
+		</div> <!-- /#mainArea -->
 	<?php 
 		$path = $_SERVER['DOCUMENT_ROOT'];
-		$path .= "/template/common/footer.php";
+		$path .= "/test/02/common/footer.php";
 		include_once($path);
 	?>
     </div> <!-- /#wrap -->
+    <script src="common/js/jQuery.js" type="text/javascript"></script>
+    <script>
+        $(document).ready(function(){
+            
+        });
+    </script>
 </body>
 </html>
