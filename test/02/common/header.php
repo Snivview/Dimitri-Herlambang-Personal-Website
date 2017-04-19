4f3164588e389c5890b1d513d3aab3980c4024ed
Snivview <dim_plymouth@yahoo.com>
2017-04-19 21:29:35 +0700

    updated the spotlight

--- test/02/common/header.php
+++ test/02/common/header.php
@@ -1,7 +1,7 @@
 <div id="headerArea" class="clearfix">
     <div class="wrap01">
-        <ul class="dispInline">
-            <li><a href="http://www.kompas.id" target="_blank">Kompas ID</a></li>
+        <ul class="dispInline" id="hNav">
+            <li><a href="http://www.kompas.id" target="_blank"><img src="/test/02/img/logo-kompas.png"></a></li>
             <li><a href="http://karier.kompas.id" target="_blank">Kompas Karier</a></li>
             <li><a href="http://klasika.kompas.id" target="_blank">Klasika</a></li>
             <li><a href="http://klasika.kompas.id" target="_blank">Advertorial</a></li>
--- test/02/index.php
+++ test/02/index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <title>The Spotlight</title>
-    <meta name="viewport" content="initial-scale=1.0, user-scalable=yes" />
+    <meta name="viewport" content="initial-scale=1.0, user-scalable=no" />
     <link href="common/css/base.css" rel="stylesheet">
 </head>
 
@@ -24,23 +24,43 @@
                 <div class="container">
                     <div class="block">
                         <div><img src="img/spot01.png"></div>
-                        <h3>Suzuki Ignis</h3>
+                        <h3>Peluncuran Suzuki Ignis</h3>
                         <p>Lebih dari sekedar LCGC, City Car lansiran Suzuki ini mencoba mengusik pasar LCGC yang sebelumnya dikuasai pabrikan Jepang lainnya.</p>
+                        <div class="readMore">
+                            <ul><li>Advertorial</li></ul>
+                        </div>
                     </div>
                     <div class="block">
                         <div><img src="img/spot02.jpg"></div>
                         <h3>Longsor Wilis</h3>
                         <p>Sebulan sudah warga lereng gunung Wilis dirundung rasa waswas akibat bergesernya tanah tempat mereka tinggal.</p>
+                        <div class="readMore">
+                            <ul><li>Dalam Negeri</li></ul>
+                        </div>
                     </div>
                     <div class="block">
                         <div><img src="img/spot03.png"></div>
                         <h3>Pilkada Banten</h3>
                         <p>Pasca sepeninggalan Ratu Atut yang diciduk lembaga KPK beberapa tahun lalu, warga banten masih dihadapkan pada sebuah pilihan sulit.</p>
+                        <div class="readMore">
+                            <ul><li>Politik</li></ul>
+                        </div>
                     </div>
                     <div class="block">
-                        <div><img src="img/spot02.jpg"></div>
-                        <h3>Longsor Wilis</h3>
-                        <p>Sebulan sudah warga lereng gunung Wilis dirundung rasa waswas akibat bergesernya tanah tempat mereka tinggal.</p>
+                        <div><img src="img/spot04.jpg"></div>
+                        <h3>Suriah Berkecamuk</h3>
+                        <p>Kondisi politik timur tengah yang selalu dalam kondisi labil, Suriah menjadi salah satu zona perang antara kelompok radikal dengan negara-negara adidaya.</p>
+                        <div class="readMore">
+                            <ul><li>Politik Internasional</li></ul>
+                        </div>
+                    </div>
+                    <div class="block">
+                        <div><img src="img/spot05.jpeg"></div>
+                        <h3>Trump Effect</h3>
+                        <p>Belum genap satu tahun, manuver Presiden negeri Paman Sam Donald John Trump dalam "mengembalikan kejayaan Amerika" sudah cukup menggemparkan elite politik dunia, lantas bagaimana dampaknya di Indonesia ?</p>
+                        <div class="readMore">
+                            <ul><li>Politik Internasional</li></ul>
+                        </div>
                     </div>
                 </div> <!-- /.container -->
             </div> <!-- /.wrap01 -->
@@ -51,11 +71,8 @@
 		include_once($path);
 	?>
     </div> <!-- /#wrap -->
-    <script src="common/js/jQuery.js" type="text/javascript"></script>
+    
     <script>
-        $(document).ready(function(){
-            
-        });
     </script>
 </body>
 </html>
