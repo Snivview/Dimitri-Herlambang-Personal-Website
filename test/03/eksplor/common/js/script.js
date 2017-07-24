52a7ad5892f7280bd11516dc16a862effb3c6640
snivview <dim_plymouth@yahoo.com>
2017-07-24 16:56:13 +0700

    minor design modification

--- test/03/eksplor/common/js/script.js
+++ test/03/eksplor/common/js/script.js
@@ -52,7 +52,7 @@ $(document).ready(function () {
 	});
 	/* night mode */
 	$("#nightMode").click(function(){
-		$(".div-image p, .link-nav a, .div-text p, .text-lead p, h3").toggleClass("night-text");
+		$(".div-image p, .div-text p, .text-lead p, h3").toggleClass("night-text");
 		$(".div-borderB").toggleClass("night-line");
 		$(".div-imageB div").toggleClass("shadow")
 		$("img").toggleClass("night-image");
